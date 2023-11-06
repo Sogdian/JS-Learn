10b24871807957a4ef091737ecd0fa4778fee7d2
sogdian <sogdian4815@mail.ru>
2023-11-06 19:17:51 +0100

    next 10

--- stylelint.config.js
+++ stylelint.config.js
@@ -25,5 +25,6 @@ module.exports = {
     'rule-empty-line-before': 'always',
     'at-rule-empty-line-before': 'always',
     'selector-class-pattern': null,
+    'font-family-name-quotes': 'always-where-recommended',
   },
 };
