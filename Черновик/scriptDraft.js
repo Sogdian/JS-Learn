c2e6cdf3d6c5cf778c68037d600c42e088d8f33b
sogdian <sogdian4815@gmail.com>
2024-01-07 19:20:53 +0100

    next

--- Черновик/scriptDraft.js
+++ Черновик/scriptDraft.js
@@ -1,17 +1,66 @@
-function Example() {
-  const [count, setCount] = React.useState(0);
-  const [name, setName] = React.useState("John");
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+// Список реактивов передаём в reagents
+// root.render(<App reagents={['air', 'earth', 'water']} />);
 
+function App([]) { //function App({ reagents })
+  const [firstCounter, setFirstCounter] = React.useState(0);
+  const [secondCounter, setSecondCounter] = React.useState(0);
+
+  const handleFirstClick = () => {
+    setFirstCounter((count) => count + 1);
+  };
+
+  const handleSecondClick = () => {
+    setSecondCounter((count) => count + 1);
+  };
+
+  const firstSelectedOption = reagents[1];
+  const secondSelectedOption = reagents[2];
+
+  // Добавьте для кнопок-переключателей класс для выбранного реактива
   return (
-    <div>
-      <p>Вы нажали {count} раз</p>
-      <p>Вас зовут {name}</p>
-      <button onClick={() => setCount(count + 1)}>
-        Нажми на меня
+    <Game>
+      <button
+        onClick={handleFirstClick}
+        className={'button button-first button-${firstSelectedOption}'}
+        type="button"
+      >
+        {firstCounter}
       </button>
-      <button onClick={() => setName("Jane")}>
-        Изменить имя
+      <button
+        onClick={handleSecondClick}
+        className={'button button-second button-${secondSelectedOption}'}
+        type="button"
+      >
+        {secondCounter}
       </button>
-    </div>
+    </Game>
+  );
+}
+
+function Game({ children }) {
+  return (
+    <section className="game">
+      <header className="game-header">
+        <h1 className="game-title">Лаборатория</h1>
+        <p className="game-description">химические опыты</p>
+      </header>
+      <img
+        className="logo"
+        src="img/atom.svg"
+        width="78"
+        height="78"
+        alt="Атом"
+      />
+      {children}
+      <img
+        className="professor"
+        src="img/professor.svg"
+        width="200"
+        height="200"
+        alt="Профессор"
+      />
+    </section>
   );
 }
