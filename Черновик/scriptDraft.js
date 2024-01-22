12d665e1a460c4c2041721cb435b9c3f7ed3eb10
sogdian <sogdian4815@gmail.com>
2024-01-22 22:24:47 +0100

    next

--- Черновик/scriptDraft.js
+++ Черновик/scriptDraft.js
@@ -1,66 +1,24 @@
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-// Список реактивов передаём в reagents
-// root.render(<App reagents={['air', 'earth', 'water']} />);
+const acrostic = [
+  'Ангел лёг у края небосклона.',
+  'Наклонившись, удивлялся безднам.',
+  'Новый мир был синим и беззвездным.',
+  'Ад молчал, не слышалось ни стона.',
+  ' ',
+  'Алой крови робкое биенье,',
+  'Хрупких рук испуг и содроганье.',
+  'Миру лав досталось в обладанье',
+  'Ангела святое отраженье.',
+  'Тесно в мире! Пусть живёт, мечтая',
+  'О любви, о грусти и о тени,',
+  'В сумраке предвечном открывая',
+  'Азбуку своих же откровений.'
+];
 
-function App([]) { //function App({ reagents })
-  const [firstCounter, setFirstCounter] = React.useState(0);
-  const [secondCounter, setSecondCounter] = React.useState(0);
+let annaAkhmatova = ''; // Объявляем переменную, в которой будет собрана итоговая строка
 
-  const handleFirstClick = () => {
-    setFirstCounter((count) => count + 1);
-  };
-
-  const handleSecondClick = () => {
-    setSecondCounter((count) => count + 1);
-  };
-
-  const firstSelectedOption = reagents[1];
-  const secondSelectedOption = reagents[2];
-
-  // Добавьте для кнопок-переключателей класс для выбранного реактива
-  return (
-    <Game>
-      <button
-        onClick={handleFirstClick}
-        className={'button button-first button-${firstSelectedOption}'}
-        type="button"
-      >
-        {firstCounter}
-      </button>
-      <button
-        onClick={handleSecondClick}
-        className={'button button-second button-${secondSelectedOption}'}
-        type="button"
-      >
-        {secondCounter}
-      </button>
-    </Game>
-  );
+for (let i = 0; i < acrostic.length; i += 1) {
+  let str1 = acrostic[i];
+  let str2 = acrostic[i][0];
+  annaAkhmatova = annaAkhmatova + acrostic[i][0];
 }
-
-function Game({ children }) {
-  return (
-    <section className="game">
-      <header className="game-header">
-        <h1 className="game-title">Лаборатория</h1>
-        <p className="game-description">химические опыты</p>
-      </header>
-      <img
-        className="logo"
-        src="img/atom.svg"
-        width="78"
-        height="78"
-        alt="Атом"
-      />
-      {children}
-      <img
-        className="professor"
-        src="img/professor.svg"
-        width="200"
-        height="200"
-        alt="Профессор"
-      />
-    </section>
-  );
-}
+console.log(annaAkhmatova);
