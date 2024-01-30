f4aef1804fd16ad87baf0be3d05a4ea7dc688386
sogdian <sogdian4815@gmail.com>
2024-01-30 18:35:43 +0100

    next

--- Черновик/scriptDraft.js
+++ Черновик/scriptDraft.js
@@ -1,7 +1,20 @@
-function getNormal(sys, dia) {
-  return ((sys >= 100 && sys <= 135) || (dia >= 60 && dia <= 85))
+function evenSum(numbers) {
+  console.log(numbers)
+  let sum = 0;
+
+  for(let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+
+    if(number % 2 === 0) {
+      sum += number;
+    }
+  }
+
+  return sum;
 }
 
-console.log(getNormal(110, 70)); // true
-console.log(getNormal(140, 75)); // false
-console.log(getNormal(98, 80)); // false
+// Правильный результат: 2 + 4 = 6
+console.log(evenSum([1, 2, 3, 4]));
+
+// Правильный результат: 2 + 10 + 16 = 28
+console.log(evenSum([5, 2, 10, 1, 99, 16]));
